1eebfc847ff20f659d9ea65b82131bd7a0cf7f89
ildar <1919mur@gmail.com>
2020-02-21 22:33:29 +0300

    //Last commit

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,13 +3,13 @@ class MainController < ApplicationController
   before_action :set_page_options
 
   def index 
-      @brands = Brand.limit(3)  
-      @hits   = Product.all.limit(8)  
-    end
+    @brands = Brand.limit(3)  
+    @hits   = Product.all.limit(8)  
+  end
 
-    def set_page_options
-      @page_title       = 'Watches Shop'
-      @page_description = 'Watces'
-      @page_keyword     = 'Man Woman Kids'
-    end
+  def set_page_options
+    @page_title       = 'Watches Shop'
+    @page_description = 'Watces'
+    @page_keyword     = 'Man Woman Kids'
+  end
 end
