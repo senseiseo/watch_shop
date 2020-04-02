89f52b7edaa40ceac4c5dadec1263f577d565816
ildar <1919mur@gmail.com>
2020-04-02 19:56:30 +0300

    // add product controller

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,6 +1,17 @@
 class ProductController < ApplicationController
+  
+  
 
   def show
+    @product = Product.find(params[:id])
+    set_page_options
+  end
+
+  attr_accessor :product
+
+  def set_page_options
+    set_meta_tags product.slice(:title, :keywords, :description)
+    add_breadcrumb name 'Home', path :root_path, title: 'Home'
   end
 
 end
