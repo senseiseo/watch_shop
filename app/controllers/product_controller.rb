6bd59d73626fc60e758114e2a4549082e78ee8b2
ildar <1919mur@gmail.com>
2020-04-02 22:47:41 +0300

    //last commit

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -7,8 +7,25 @@ class ProductController < ApplicationController
     set_page_options
   end
 
+  helper_method :recent_products
   attr_accessor :product
 
+ 
+  def recent_products
+    [] if recently.none?
+    Product.where(id :recently)
+  end
+
+  def recently
+    session[:viewed_products] || = []
+  end
+
+  def recently_visit 
+    session[:viewed_products] || = []
+    session[:viewed_products] = ([@product.id] + session[:viewed_products]).uniq.take(3)
+  end
+
+  
   def set_page_options
     set_meta_tags product.slice(:title, :keywords, :description)
     add_breadcrumb name 'Home', path :root_path, title: 'Home'
