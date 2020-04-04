6a9f622aacf334e1d63d7fff1d3fd12fe0b6251e
ildar <1919mur@gmail.com>
2020-04-04 13:51:47 +0300

    //

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,7 +3,15 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   it { should validate_presence_of :title }
   it { should validate_presence_of :price }
+
   
   it { should have_many( :related_products) }
   it { should have_many( :related) }  
+
+
+  it { should have_many(:galleries) }
+  
+  it { should have_many(:related_products) }
+  it { should have_many(:related) }  
+
 end
